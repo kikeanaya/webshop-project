7577921c4f61f666253bc065b410ae4672f8df85
kikeanaya <luiseanayam@gmail.com>
2019-05-13 11:18:01 +0200

    implemented remove button and changed cart display

--- src/App.js
+++ src/App.js
@@ -3,31 +3,31 @@ import github from './Images/github.svg'
 import linkedin from './Images/linkedin.svg'
 import { connect } from 'react-redux';
 
+
 const applyFilter = searchTerm => article =>
   article.name.toLowerCase().includes(searchTerm.toLowerCase()) ||article.brewery.toLowerCase().includes(searchTerm.toLowerCase()) ;
 
-const App = ({ articles, searchTerm, cart, onSearch, addBeer }) =>
+const App = ({ articles, searchTerm, cart, onSearch, addBeer, removeBeer, cartOpen, openCart, closeCart}) =>
   <div className="app">
     <Search searchTerm={searchTerm} onSearch={onSearch}>
     </Search>
-    <Cart cart={cart}/>
+    <Cart cart={cart} removeBeer={removeBeer} openCart={openCart} cartOpen={cartOpen} closeCart={closeCart}/>
     <Articles articles={articles.filter(applyFilter(searchTerm))} addBeer={addBeer} />
     <Footer/>
   </div>
 
-const Cart = ({ cart }) =>
+const Cart = ({ cart, removeBeer, openCart, cartOpen, closeCart }) =>
+
       <div>
-       
-        
-          <button className= {(cart.length>0) ? "cart-icon" : "cart-icon-hidden"}>
+          <button className= {(cart.length>0) ? "cart-icon" : "cart-icon-hidden"} onClick= {() => openCart()}>
             <span className="cart-quantity">{cart.length}</span>
           </button>
-    
-        <div className="entire-cart">
+        <div className={(cartOpen === true) ? "entire-cart cart-open" : "entire-cart"}>
+          <button className="close-icon" onClick= {() => closeCart()}></button>
           <h2>YOUR CART:</h2>
           {
             cart.map(beer=>
-              <BeerInCart beer={beer} key={beer.id}/>
+              <BeerInCart beer={beer} key={beer.id} removeBeer={removeBeer}/>
             )
           }
           <p>You have {cart.length} beers in your cart.</p>
@@ -36,10 +36,11 @@ const Cart = ({ cart }) =>
         
       </div>
 
-const BeerInCart = ({beer}) =>
+const BeerInCart = ({beer, removeBeer}) =>
     <div className="cart-beer">
       <p>{beer.name}</p>
       <img src={require(`./Images/beer-images/${beer.image}`)} alt={beer.name}/>
+      <button onClick={() => removeBeer(beer)}></button>
     </div>
 
 const Search = ({ searchTerm, onSearch}) =>
@@ -98,12 +99,16 @@ const Footer = () =>
 const mapStateToProps = state => ({
   articles: state.articlesState.articles,
   searchTerm: state.searchState.searchTerm,
-  cart: state.cartState.cart
+  cart: state.cartState.cart,
+  cartOpen: state.cartState.cartOpen
 });
 
 const mapDispatchToProps = dispatch => ({
   onSearch: searchTerm => dispatch({ type: 'SEARCH_SET', searchTerm }),
-  addBeer: beerToAdd => dispatch({ type: 'ADD_BEER', beerToAdd })
+  addBeer: beerToAdd => dispatch({ type: 'ADD_BEER', beerToAdd }),
+  removeBeer: beerToRemove => dispatch({ type: 'REMOVE_BEER', beerToRemove }),
+  openCart: () => dispatch({ type: 'OPEN_CART' }),
+  closeCart: () => dispatch({ type: 'CLOSE_CART' })
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/Reducers/cartReducer.js
+++ src/Reducers/cartReducer.js
@@ -1,11 +1,18 @@
 const INITIAL_STATE = {
-    cart: []
+    cart: [],
+    cartOpen: false
   };
   
   function cartReducer(state = INITIAL_STATE, action) {
     switch (action.type) {
       case 'ADD_BEER':
         return { ...state, cart: [...state.cart, action.beerToAdd ]};
+      case 'REMOVE_BEER':
+        return { ...state, cart: state.cart.filter(beer=>beer.name !== action.beerToRemove.name)};
+      case 'OPEN_CART':
+        return { ...state, cartOpen: true};
+      case 'CLOSE_CART':
+        return { ...state, cartOpen: false};
       default: return state;
     }
   }
